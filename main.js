d16885fe6eabc284eae9a04749c379161499b389
pooiod7 <portsmouthuser35@gmail.com>
2026-02-03 08:59:23 -0500

    o

--- main.js
+++ main.js
@@ -1,73 +1,74 @@
 var swf = document.querySelector('#scratch embed');
 
-// Scratch.FlashApp.ASobj.ASloadGithubURL(extensionUrl);
+function getQueryParam(name) {
+    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
 
 window.gotZipBase64 = function(content) {
-    document.getElementById("loadholder").classList.add("pulse");
+    $("#loadholder").addClass("pulse");
 
-    $("#log").text("Waiting for flash hook"+"\n"+$("#log").text());
+    $("#log").text("Waiting for flash hook" + "\n" + $("#log").text());
     var tries = 0;
-    var openTimeout = setInterval(()=>{
+    
+    var openTimeout = setInterval(function() {
         swf = document.querySelector('#scratch embed');
         tries += 1;
 
-        console.log(tries);
         if (swf && swf.ASopenProjectFromData) {
-            $("#log").text("Opening project"+"\n"+$("#log").text());
+            $("#log").text("Opening project" + "\n" + $("#log").text());
             clearInterval(openTimeout);
             swf.ASopenProjectFromData(content);
-            setTimeout(() => {
+            
+            setTimeout(function() {
                 $("#scratchloader").css("opacity", 0);
-                $('#downloader').animate({height: 0}, 1000);
+                $('#downloader').animate({ height: 0 }, 1000);
             }, 500);
         } else if (tries >= 10) {
             clearInterval(openTimeout);
-            $("#log").text("Hook not found"+"\n"+$("#log").text());
-            if (!hasFlash) {
+            $("#log").text("Hook not found" + "\n" + $("#log").text());
+            
+            if (typeof hasFlash !== "undefined" && !hasFlash) {
                 if (confirm("Download this project?")) {
                     JSdownloadSB2(content, window.DownloadedTitle || "project.sb2");
                 } else {
                     alert("Please download this modified scratch 2.0 app, or a browser that supports flash");
                     location.href = "https://pooiod7.itch.io/scratch2";
                 }
-
-                // history.replaceState(null,'',`${location.pathname}?id=${location.hash.slice(1)}`);
-                // location.href = `https://ie10.ieonchrome.com/#${location.href}`; // Does not work because of js things
-            } else {
-                title = "Scratch2 - " + DownloadedTitle;
             }
-            setTimeout(() => {
+            
+            setTimeout(function() {
                 $("#scratchloader").css("opacity", 0);
-                $('#downloader').animate({height: 0}, 1000);
+                $('#downloader').animate({ height: 0 }, 1000);
             }, 1000);
         }
     }, 1000);
 };
 
-// $("#log").text("");
-// $("#progress").removeClass("error success");
-// $("#progress").css("opacity", 1);
-// $("#log").text("value"+"\n"+$("#log").text());
-// document.getElementById("downloader").style.height = "100px";
-// document.getElementById("downloader").style.display = "block";
+var value = getQueryParam('id') || decodeURIComponent(location.hash.slice(1));
 
-var value = new URLSearchParams(window.location.search).get('id') || decodeURI(location.hash.slice(1));
-
-if (value.includes('#')) {
+if (value.indexOf('#') !== -1) {
     value = value.split('#').pop();
-} else if (value.startsWith('http')) {
-    value = value
-        .split('/')
-        .find(part => /^\d+$/.test(part)) || '';
+} 
+else if (value.indexOf('http') === 0) {
+    var parts = value.split('/');
+    var foundPart = '';
+    for (var i = 0; i < parts.length; i++) {
+        if (/^\d+$/.test(parts[i])) {
+            foundPart = parts[i];
+            break;
+        }
+    }
+    value = foundPart;
 }
 
-if (JSwillDownload() && value) {
-    document.body.classList.add('download');
+if (typeof JSwillDownload === "function" && JSwillDownload() && value) {
+    $("body").addClass('download');
     startDownload(value);
 } else {
-    var value2 = new URLSearchParams(window.location.search).get('project_url')
-    if (JSwillDownload() && value2) {
-        document.body.classList.add('download');
+    var value2 = getQueryParam('project_url');
+    if (typeof JSwillDownload === "function" && JSwillDownload() && value2) {
+        $("body").addClass('download');
         startDownload(value2);
     }
 }
