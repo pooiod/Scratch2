8817bf2c87c1ad31d762f82e5f8e1daed0ee591e
pooiod7 <portsmouthuser35@gmail.com>
2026-02-03 20:24:33 -0500

    f

--- scriptTranspiler.js
+++ scriptTranspiler.js
@@ -25,79 +25,73 @@
 
     function loadFiles(cb) {
         var remaining = files.length;
-
-        files.forEach(function (file) {
-            load(file, function () {
+        for (var i = 0; i < files.length; i++) {
+            load(files[i], function () {
                 remaining--;
-                if (remaining === 0) {
-                    cb();
-                }
+                if (remaining === 0) cb();
             });
-        });
+        }
     }
 
     function transpileFiles(cb) {
         var remaining = files.length;
+        for (var i = 0; i < files.length; i++) {
+            (function(fileUrl) {
+                get(fileUrl, function (code) {
+                    try {
+                        var ieFunc = "function svgToPng(svgText) { " +
+                            "return new Promise(function(resolve, reject) { " +
+                            "var c = document.createElement('canvas'); " +
+                            "c.width = typeof outW !== 'undefined' ? outW : 300; " +
+                            "c.height = typeof outH !== 'undefined' ? outH : 150; " +
+                            "canvg(c, svgText, { " +
+                            "ignoreMouse: true, " +
+                            "ignoreAnimation: true, " +
+                            "renderCallback: function() { " +
+                            "c.toBlob(function(blob) { " +
+                            "var fr = new FileReader(); " +
+                            "fr.onload = function() { resolve(new Uint8Array(fr.result)); }; " +
+                            "fr.onerror = function(e) { reject(e); }; " +
+                            "fr.readAsArrayBuffer(blob); " +
+                            "}, 'image/png'); " +
+                            "} " +
+                            "}); " +
+                            "}); " +
+                            "}";
 
-        files.forEach(function (file) {
-            get(file, function (code) {
-                console.log("Transpiling " + file);
-                try {
-                    var ieFunc = "async function svgToPng(svgText) { " +
-                        "return new Promise(function(resolve, reject) { " +
-                        "var c = document.createElement('canvas'); " +
-                        "c.width = outW; c.height = outH; " +
-                        "canvg(c, svgText, { " +
-                        "ignoreMouse: true, " +
-                        "ignoreAnimation: true, " +
-                        "renderCallback: function() { " +
-                        "c.toBlob(function(blob) { " +
-                        "var fr = new FileReader(); " +
-                        "fr.onload = function() { resolve(new Uint8Array(fr.result)); }; " +
-                        "fr.onerror = function(e) { reject(e); }; " +
-                        "fr.readAsArrayBuffer(blob); " +
-                        "}, 'image/png'); " +
-                        "} " +
-                        "}); " +
-                        "}); " +
-                        "}";
+                        var re = new RegExp("async\\s+function\\s+svgToPng\\s*\\(svgText\\)\\s*\\{[\\s\\S]*?return\\s+new\\s+Uint8Array\\(ab\\);\\s*\\}", "g");
+                        code = code.replace(re, ieFunc);
 
-                    code = code.replace(/async function svgToPng\(svgText\) \{[\s\S]*?return new Uint8Array\(ab\);\s*\}/, ieFunc);
-                } catch (e) {
-                    if (console && console.error) console.error(e);
-                }
+                        var transpiled = Babel.transform(code, { 
+                            presets: ['es2015', 'stage-2'] 
+                        }).code;
 
-                var transpiled = Babel.transform(code, { presets: ['es2015'] }).code;
+                        var s = document.createElement('script');
+                        s.type = 'text/javascript';
+                        s.text = transpiled;
+                        document.getElementsByTagName('head')[0].appendChild(s);
+                    } catch (e) {
+                        if (console && console.error) console.error("Error in " + fileUrl + ": ", e);
+                    }
 
-                var s = document.createElement('script');
-                s.type = 'text/javascript';
-                s.async = false;
-                s.text = transpiled;
-                document.getElementsByTagName('head')[0].appendChild(s);
-
-                remaining--;
-                if (remaining === 0) {
-                    cb();
-                }
-            });
-        });
+                    remaining--;
+                    if (remaining === 0) cb();
+                });
+            })(files[i]);
+        }
     }
 
-    // I didn't want to reqrite all my code to be IE compatible, so libs are used instead
-    if (true || /MSIE|Trident/.test(navigator.userAgent)) {
-        console.log("Preparing libs for IE");
+    if (/MSIE|Trident/.test(navigator.userAgent)) {
         load('https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.26.0/polyfill.min.js', function () {
             load('https://cdn.jsdelivr.net/npm/whatwg-fetch@3.0.0/dist/fetch.umd.min.js', function () {
                 load('https://cdn.jsdelivr.net/npm/fast-text-encoding@1.0.6/text.min.js', function () {
                     load('https://cdn.jsdelivr.net/npm/canvas-toBlob@1.0.0/canvas-toBlob.min.js', function () {
                         load('https://cdnjs.cloudflare.com/ajax/libs/canvg/1.4/rgbcolor.min.js', function () {
                             load('https://cdnjs.cloudflare.com/ajax/libs/stackblur-canvas/1.4.1/stackblur.min.js', function () {
-                                load('https://cdn.jsdelivr.net/npm/canvg@1.5.3/dist/browser/canvg.min.js', function () {
+                                load('https://cdnjs.cloudflare.com/ajax/libs/canvg/1.5.3/canvg.min.js', function () {
                                     load('https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.26.0/babel.min.js', function () {
-                                        console.log("Transpiling scripts for IE");
                                         transpileFiles(function() {
-                                            console.log("Scripts transpiled :D");
-                                            startMain();
+                                            if (window.startMain) window.startMain();
                                         });
                                     });
                                 });
@@ -109,7 +103,7 @@
         });
     } else {
         loadFiles(function() {
-            startMain();
+            if (window.startMain) window.startMain();
         });
     }
 })(['/download.js', '/scratchGUI.js']);
