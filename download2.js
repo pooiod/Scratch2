0a8e6b47f202b92a5abc8af95581fb7cfa6c2241
pooiod7 <portsmouthuser35@gmail.com>
2026-02-03 19:14:29 -0500

    f

--- download2.js
+++ download2.js
@@ -1,65 +1,45 @@
 (function () {
-    function loadScript(src, callback) {
-        var script = document.createElement('script');
-        script.src = src;
-        script.async = false;
-        script.onload = script.onreadystatechange = function () {
+    function load(src, cb) {
+        var s = document.createElement('script');
+        s.src = src;
+        s.async = false;
+        s.onload = s.onreadystatechange = function () {
             if (!this.readyState || this.readyState === 'loaded' || this.readyState === 'complete') {
-                script.onload = script.onreadystatechange = null;
-                if (callback) callback();
+                s.onload = s.onreadystatechange = null;
+                if (cb) cb();
             }
         };
-        document.head.appendChild(script);
+        document.getElementsByTagName('head')[0].appendChild(s);
     }
 
-    function fetchSource(url, callback) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', url, true);
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200 || xhr.status === 0) {
-                    callback(xhr.responseText);
-                } else {
-                    alert('Failed to fetch ' + url);
-                }
+    function get(url, cb) {
+        var x = new XMLHttpRequest();
+        x.open('GET', url, true);
+        x.onreadystatechange = function () {
+            if (x.readyState === 4 && (x.status === 200 || x.status === 0)) {
+                cb(x.responseText);
             }
         };
-        xhr.send(null);
+        x.send(null);
     }
 
-    function initLoader() {
-        loadScript('https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.26.0/polyfill.min.js', function () {
-            loadScript('https://cdn.jsdelivr.net/npm/whatwg-fetch@3.0.0/dist/fetch.umd.min.js', function () {
-                loadScript('https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.26.0/babel.min.js', function () {
-
-                    fetchSource('download.js', function (modernCode) {
+    load('https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.26.0/polyfill.min.js', function () {
+        load('https://cdn.jsdelivr.net/npm/whatwg-fetch@3.0.0/dist/fetch.umd.min.js', function () {
+            load('https://cdn.jsdelivr.net/npm/fast-text-encoding@1.0.6/text.min.js', function () {
+                load('https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.26.0/babel.min.js', function () {
+                    get('download.js', function (code) {
                         try {
-                            if (typeof Babel === 'undefined' || !Babel.transform) {
-                                throw new Error('Babel compiler failed to load.');
-                            }
-
-                            var result = Babel.transform(modernCode, {
-                                presets: ['es2015', 'stage-2']
-                            });
-
-                            var newScript = document.createElement('script');
-                            newScript.type = 'text/javascript';
-                            newScript.text = result.code;
-                            document.body.appendChild(newScript);
-
-                            if (window.startMain) {
-                                window.startMain();
-                            }
-
+                            var out = Babel.transform(code, { presets: ['es2015', 'stage-2'] }).code;
+                            var s = document.createElement('script');
+                            s.text = out;
+                            document.body.appendChild(s);
+                            window.startMain();
                         } catch (e) {
-                            alert('Error: ' + e.message);
                             if (console && console.error) console.error(e);
                         }
                     });
                 });
             });
         });
-    }
-
-    initLoader();
+    });
 })();
