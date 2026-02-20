d3998eb976703fe515c10996e854c072e8313a49
pooiod7 <portsmouthuser35@gmail.com>
2026-02-20 14:17:47 -0500

    Fix query selector for SWF and update functions
    supid ahh ai comit message thingy

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var swf = document.querySelector('#scratch embed') || document.querySelector('ruffle-playe');
+var swf = document.querySelector('#scratch embed') || document.querySelector('ruffle-player');
 
 function getQueryParam(name) {
     var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
@@ -12,7 +12,7 @@ window.gotZipBase64 = function(content) {
     var tries = 0;
     
     var openTimeout = setInterval(function() {
-        swf = document.querySelector('#scratch embed') || document.querySelector('ruffle-playe');
+        swf = document.querySelector('#scratch embed') || document.querySelector('ruffle-player');
         tries += 1;
 
         if (swf && swf.ASopenProjectFromData) {
@@ -93,4 +93,5 @@ window.startMain = function() {
         }
     }
 };
+
 
